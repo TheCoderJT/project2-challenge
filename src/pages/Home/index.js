93cc6bf9e1f24838b0ad5ef9cc69022c123e9129
Jordan Turner <gamerslife56@gmail.com>
2021-09-26 15:36:28 -0600

    adjusted endpoint paths

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,15 +13,15 @@ const Home = () => {
     <Router>
       <Header />
       <Switch>
-        <Route path='/' exact>
+        <Route path='/project2-challenge/' exact>
           {/* load post data */}
           <Users />
         </Route>
-        <Route path='/posts' exact>
+        <Route path='/project2-challenge/posts' exact>
           {/* load post data */}
           <Posts />
         </Route>
-        <Route path='/comments' exact>
+        <Route path='/project2-challenge/comments' exact>
           {/* load comment data */}
           <Comments />
         </Route>
