74a5e082ccc83b3c13bbddb63d1898d613efbfaa
Jordan Turner <gamerslife56@gmail.com>
2021-09-26 17:03:38 -0600

    reverted back to calling fetch hook 3 times

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // styles
 import Header from "../../components/Header";
@@ -8,23 +8,20 @@ import Users from "../Users";
 import Posts from "../Posts";
 import Comments from "../Comments";
 
-//* I have no idea if this works, but you would think
-//* it loads the home route on load...
 const Home = () => {
-  useEffect(() => {
-    <Route path='/' exact>
-      {/* load post data */}
-      <Users />
-    </Route>;
-  }, []);
-
   return (
     <Router basename={"/project2-challenge"}>
       <Header />
       <Switch>
-        <Route path='/' exact component={Users} />
-        <Route path='/posts' exact component={Posts} />
-        <Route path='/comments' exact component={Comments} />
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/posts' exact>
+          <Posts />
+        </Route>
+        <Route path='/comments' exact>
+          <Comments />
+        </Route>
       </Switch>
     </Router>
   );
--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // styles
 import { Wrapper, Content } from "./Users.styles";
